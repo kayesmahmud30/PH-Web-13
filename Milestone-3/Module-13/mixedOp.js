8d581bd1ae5efc039760298943504ab9d99e9b36
Kayes Mahmud <mahmudkayes30@gmail.com>
2026-01-23 22:19:41 +0600

    Added new practice files

---
+++ Milestone-3/Module-13/mixedOp.js
@@ -0,0 +1,10 @@
+var first = "Kayes";
+var second = "Mahmud";
+
+var fullName = first + second;
+
+// console.log(fullName);
+// console.log('40'+2);
+console.log(40-'20');
+console.log('50'-10);
+
---
+++ Milestone-3/Module-13/shortHand.js
@@ -0,0 +1,17 @@
+var price = 35;
+// price = price + 10;
+price += 10;
+// console.log(price);
+
+var fridge = 35000;
+// fridge = fridge - 1000;
+fridge -= 1000;
+// console.log(fridge);
+
+//Multiply
+
+var orangPrice = 30;
+// orangPrice = orangPrice * 3;
+orangPrice *= 3;
+console.log(orangPrice);
+
