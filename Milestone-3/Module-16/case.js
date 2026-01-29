94c204f1f465708030c0d913103b53405114abf2
Kayes Mahmud <mahmudkayes30@gmail.com>
2026-01-30 02:32:41 +0600

    Added some ppractice files

---
+++ Milestone-3/Module-16/case.js
@@ -0,0 +1,23 @@
+const school = "RAJ UK Uttara Model School";
+
+console.log(school);
+console.log(school.toLocaleLowerCase());
+console.log(school.toUpperCase());
+
+//Uppercase....A B C D...
+//Lowercase....a b c d...
+
+const subject = "Chemistry";
+const book = "ChemIsTry";
+
+if (subject.toLowerCase() === book.toLocaleLowerCase()) {
+  console.log("I will make a good result");
+} else {
+  console.log("Nothing good at all, Just eat & sleep");
+}
+
+const drink = "water ";
+const liquid = "   water     ";
+
+if (drink === liquid) {
+}
---
+++ Milestone-3/Module-16/string.js
@@ -0,0 +1,25 @@
+const country = "Bangladesh";
+const division = "Newkhale";
+const district = `B-Baria`;
+
+// const thana = new String(`Demra`)
+// console.log(thana);
+
+const numbers = [12, 54, 67, 87, 98];
+console.log(numbers);
+
+// console.log(numbers.length);
+// console.log(numbers[1]);
+
+numbers[1] = 11;
+console.log(numbers);
+
+//String is immutable data.....Not Changeable.
+//Array is mutable data.....Changeable.
+
+const capital = "Dhaka";
+// console.log(capital.length);
+// console.log(capital[3]);
+console.log(capital);
+capital[0] = "F";
+console.log(capital);
