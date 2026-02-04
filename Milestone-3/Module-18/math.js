f74dd622153153c2be550a3e3f0c41d5f8765044
Kayes Mahmud <mahmudkayes30@gmail.com>
2026-02-05 01:53:52 +0600

    Added all the practices for Module-18

---
+++ Milestone-3/Module-18/math.js
@@ -0,0 +1,24 @@
+// const Math = {
+//   min: function min(num1, num2) {
+//     return num2;
+//   },
+// };
+
+// console.log(Math.max);
+
+const min = Math.min(45, 65, 76, 89, 98, 65, 67, 4, -88);
+console.log(min);
+const max = Math.max(45, 65, 76, 89, 98, 65, 67, 4, -88, 997);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(-88));
+console.log(Math.round(2.25));
+console.log(Math.round(2.85));
+console.log(Math.floor(2.85));
+console.log(Math.ceil(2.85));
+
+console.log(Math.random());
+console.log(Math.random() * 10);
+const rand = Math.round(Math.random() * 10);
+console.log(rand);
