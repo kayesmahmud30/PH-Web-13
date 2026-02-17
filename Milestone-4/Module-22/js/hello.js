ae8910e3d89e6568e1b89bfd5bad068ff9370a27
Kayes Mahmud <mahmudkayes30@gmail.com>
2026-02-18 00:57:47 +0600

    Added some new lines of code for DOM & added a folder

--- Milestone-4/Module-22/js/hello.js
+++ Milestone-4/Module-22/js/hello.js
@@ -1,2 +1,13 @@
 console.log("Hello from outside");
 console.log(document);
+
+/*      --------------------------Important-------------------
+
+      1. getAttribute('attribute-name')
+      2. setAttribute('attribute-name', 'attribute-value that we want to set for the attribute')
+
+      3. classList.add()
+      4. classList.remove()
+
+        --------------------------Important-------------------
+       */
