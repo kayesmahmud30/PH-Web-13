e7dacfed31a71c19bc3b06f9edaf94212764589c
Kayes Mahmud <mahmudkayes30@gmail.com>
2026-02-25 14:39:27 +0600

    Added file for Milestone-4 's 25.5 Module

---
+++ Milestone-4/Module-25.5/validation.js
@@ -0,0 +1,16 @@
+const number = 23;
+// String, Number, Boolean
+
+if (typeof number == "number") {
+  console.log("Value is a number");
+} else {
+  console.log("This is not a number");
+}
+
+const numbers = [12, 75, 35, 37, 56];
+const student = { name: "Rongila", job: "Khoa daoa koea" };
+// console.log(Array.isArray(numbers));
+console.log(Array.isArray(number));
+console.log(typeof student);
+
+console.log(isNaN([]));
---
+++ Milestone-4/recap.js
@@ -0,0 +1,26 @@
+/*
+1. How to use js with html and css.
+2. Get elements from DOM
+    2-1. getElementById
+    2-2. getElementsByClassName
+    2-3. querySelector
+    2-4. querySelectorAll
+
+3. Dynamically add elements to the DOM: document.createElement
+    3:5. Set value to the innerHTML
+
+4. Get and set value from/to a DOM elements.
+    4-1. Use innerText (h1, p, h5, div, span, strong)
+    4-2: Use value (input, textArea)
+
+5. Add event listener (Event handler)
+    5-1. click, focus, blur, on change, keyup
+    5-2. addEventListener onclick = "myHandler()"
+
+6. Use function to reduce duplicate code.
+
+7. Event bubble and Event delegate.
+
+*/
+
+document.querySelectorAll(".usersContainer .disable h3");
