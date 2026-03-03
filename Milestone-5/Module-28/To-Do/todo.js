a79aa49b89f08cf86afde12c1a8998c518c67b49
Kayes Mahmud <mahmudkayes30@gmail.com>
2026-03-03 15:50:49 +0600

    Added all files & folders for Module 28's 0-8 no. video

---
+++ Milestone-5/Module-28/To-Do/todo.js
@@ -0,0 +1,36 @@
+//----------------------Module-28-(7)------------------------------
+
+const loadTodo = () => {
+  const url = "https://jsonplaceholder.typicode.com/todos";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayTodo(data));
+};
+
+// {
+//     "userId": 7,
+//     "id": 129,
+//     "title": "rerum culpa quis harum",
+//     "completed": false
+// }
+
+const displayTodo = (todos) => {
+  const todoContainer = document.getElementById("todo-container");
+  todoContainer.innerHTML = "";
+
+  todos.forEach((todo) => {
+    const todoCard = document.createElement("div");
+
+    todoCard.innerHTML = `
+
+    <div class="todo-card">
+        <p>${todo.completed == true ? `<i class="fa-solid fa-square-check"></i>` : `<i class="fa-regular fa-square-check"></i> `}</p>
+        <h4>${todo.title}</h4>
+    </div>
+    
+    `;
+    todoContainer.append(todoCard);
+  });
+};
+
+loadTodo();
