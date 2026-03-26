402aeaaa82f26b9125bdfea862e0a40bf100abd6
Kayes Mahmud <mahmudkayes30@gmail.com>
2026-03-27 03:34:31 +0600

    Added all code of first 3 videos of Module 34

---
+++ Milestone-6/Module-34/tailwind-react-pricing-options/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import Link from "./Link";
+import { Menu, X } from "lucide-react";
+
+const navigationData = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/",
+  },
+  {
+    id: 2,
+    name: "About",
+    path: "/about",
+  },
+  {
+    id: 3,
+    name: "Services",
+    path: "/services",
+  },
+  {
+    id: 4,
+    name: "Blog",
+    path: "/blog",
+  },
+  {
+    id: 5,
+    name: "Contact",
+    path: "/contact",
+  },
+];
+
+const NavBar = () => {
+  const [open, setOpen] = useState(false);
+  const links = navigationData.map((route) => (
+    <Link key={route.id} route={route}></Link>
+  ));
+  return (
+    <nav className="flex justify-between mx-10">
+      <span
+        className="flex"
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        {open ? (
+          <X className="md:hidden"></X>
+        ) : (
+          <Menu className="md:hidden"></Menu>
+        )}
+
+        <ul className="md:hidden">{links}</ul>
+
+        <h3 className="ml-4">My Navbar</h3>
+      </span>
+      <ul className="md:flex hidden">{links}</ul>
+
+      {/* <ul className="flex">
+        {navigationData.map((route) => (
+          <li className="mr-10">
+            <a href={route.path}>{route.name}</a>
+          </li>
+        ))}
+      </ul> */}
+
+      {/* <ul className='flex'>
+                <li className='mr-10'><a href="/">Home</a></li>
+                <li className='mr-10'><a href="/about">About</a></li>
+                <li className='mr-10'><a href="/blog">Blog</a></li>
+            </ul> */}
+
+      <button>Sign IN</button>
+    </nav>
+  );
+};
+
+export default NavBar;
