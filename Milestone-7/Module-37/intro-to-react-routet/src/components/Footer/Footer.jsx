c60aae17ef30f28e5c82aa047580412412057452
Kayes Mahmud <mahmudkayes30@gmail.com>
2026-04-09 22:51:57 +0600

    Added a new module's file and folders

---
+++ Milestone-7/Module-37/intro-to-react-routet/src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>
+        <small>Thank You for Visiting Our web site</small>
+        <a href="/">Terms</a>
+        <a href="/">Privacy</a>
+        <a href="/fb">Facebook</a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
