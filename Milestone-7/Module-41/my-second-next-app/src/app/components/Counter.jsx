f1c5c6537d260fcacfb57a44191e978476b88a91
Kayes Mahmud <mahmudkayes30@gmail.com>
2026-04-20 11:03:27 +0600

    Added some new code

---
+++ Milestone-7/Module-41/my-second-next-app/src/app/components/Counter.jsx
@@ -0,0 +1,17 @@
+"use client";
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  console.log("Counter value in counter component", count              );
+
+  return (
+    <div>
+      <h2 className="text-5xl">Counter: {count} </h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+};
+
+export default Counter;
---
+++ Milestone-7/Module-41/my-second-next-app/src/app/components/Header.jsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+  console.log("Header Component");
+
+  return (
+    <div>
+      <h2 className="text-6xl">My awesome header</h2>
+    </div>
+  );
+};
+
+export default Header;
