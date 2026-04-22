b27d65a7e7e73511854312c4134242dbd97b86c1
Kayes Mahmud <mahmudkayes30@gmail.com>
2026-04-22 08:31:05 +0600

    Added again some new files and flolders for the module 41

--- Milestone-7/Module-41/my-second-next-app/src/app/foods/[foodId]/page.jsx
+++ Milestone-7/Module-41/my-second-next-app/src/app/foods/[foodId]/page.jsx
@@ -7,7 +7,13 @@ const FoodDetailPage = async ({ params }) => {
     `https://phi-lab-server.vercel.app/api/v1/lab/foods/${foodId}`,
   );
   const data = await res.json();
-  const { id, dish_name, image_link } = data.data;
+  const {
+    id,
+    dish_name,
+    image_link,
+    main_ingredients,
+    approximate_nutrition_per_serving,
+  } = data.data;
 
   //   console.log("Food in food Page", food);
 
@@ -15,7 +21,32 @@ const FoodDetailPage = async ({ params }) => {
     <div>
       <h2>Sowing details of food: {foodId}</h2>
       <h3 className="text-3xl">{dish_name}</h3>
-      <Image src={image_link} alt={dish_name} width={300} height={300}></Image>
+      <Image
+        className="mx-auto"
+        src={image_link}
+        alt={dish_name}
+        width={300}
+        height={300}
+      ></Image>
+      <h2 className="text-2xl">Ingredients:</h2>
+      <ul className="list-disc">
+        {main_ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
+
+      <br />
+
+      <h2 className="text-2xl">Approximate Nutrition per Serving:</h2>
+      <ul className="list-disc">
+        {Object.entries(approximate_nutrition_per_serving).map(
+          ([key, value]) => (
+            <li className="capitalize" key={key}>
+              {key} : {value}
+            </li>
+          ),
+        )}
+      </ul>
     </div>
   );
 };
