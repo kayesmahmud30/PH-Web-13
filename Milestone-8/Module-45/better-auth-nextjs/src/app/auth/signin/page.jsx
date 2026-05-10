b65fd3fdcc78a422969d311ddaf34c641965e177
Kayes Mahmud <mahmudkayes30@gmail.com>
2026-05-11 03:18:56 +0600

    Added Module-45's all code (Fully)

--- Milestone-8/Module-45/better-auth-nextjs/src/app/auth/signin/page.jsx
+++ Milestone-8/Module-45/better-auth-nextjs/src/app/auth/signin/page.jsx
@@ -24,7 +24,7 @@ const SignInPage = () => {
       email: userData.email,
       password: userData.password,
       rememberMe: true,
-      callbackURL: "/",
+      callbackURL: "/dashboard",
     });
     console.log("Sign in response:", { data, error });
   };
--- Milestone-8/Module-45/better-auth-nextjs/src/app/dashboard/page.jsx
+++ Milestone-8/Module-45/better-auth-nextjs/src/app/dashboard/page.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  console.log("Session data in dashboard", session);
+  const user = session?.user;
+
+  if (!user) {
+    redirect("/auth/signin");
+    return <div>Please sign in to the access to dashboard</div>;
+  }
+
   return (
     <div>
       <h2>This is Dashing Board</h2>
