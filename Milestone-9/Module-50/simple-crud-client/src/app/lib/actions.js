abace76e211e76d3f1b202962516ded30c9ebb6d
Kayes Mahmud <mahmudkayes30@gmail.com>
2026-05-16 19:52:16 +0600

    Added the all codes of practice of Module-50

--- Milestone-9/Module-50/simple-crud-client/src/app/lib/actions.js
+++ Milestone-9/Module-50/simple-crud-client/src/app/lib/actions.js
@@ -1,4 +1,5 @@
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export const createUser = async (formData) => {
   "use server";
@@ -18,6 +19,28 @@ export const createUser = async (formData) => {
   }
 
   return data;
+};
+
+export const updateUser = async (userId, formData) => {
+  "use server";
+  const updatedUser = Object.fromEntries(formData.entries());
+
+  const res = await fetch(`http://localhost:5000/users/${userId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updatedUser),
+  });
+
+  const data = await res.json();
+  console.log("After Update", data);
+
+  // TODO: revalidation
+  if (data.modifiedCount > 0) {
+    revalidatePath("/users");
+    redirect("/users");
+  }
 };
 
 export const deleteUser = async (userId) => {
